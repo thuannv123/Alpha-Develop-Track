685ead48536574a134caaf992f553a9a4567f163
Thuan Nguyen <thuannv06022001@gmail.com>
2026-01-30 22:13:46 +0700

    push

---
+++ asset/js/slideder.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+  $(".video_view").slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 5.1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3.5,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+  $(".slide_post").slick({
+    dots: false,
+    infinite: true,
+
+    slidesToShow: 3.6,
+    slidesToScroll: 1,
+
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 8000,
+    cssEase: "linear",
+
+    pauseOnHover: true,
+    pauseOnFocus: true,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1.2,
+        },
+      },
+    ],
+  });
+});
