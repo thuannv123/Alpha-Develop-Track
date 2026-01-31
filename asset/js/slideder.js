20631dad4c56cb762b9e5fec2c65ac39bc6e928a
Thuan Nguyen <thuannv06022001@gmail.com>
2026-01-31 08:33:59 +0700

    chazo

--- asset/js/slideder.js
+++ asset/js/slideder.js
@@ -67,4 +67,36 @@ $(document).ready(function () {
       },
     ],
   });
+  $(".slide_list").slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3.5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1.1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 });
