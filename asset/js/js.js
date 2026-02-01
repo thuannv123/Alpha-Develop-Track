e092c28c588107a9a1bc25e2fcbf5d5ed5d4a028
Thuan Nguyen <thuannv06022001@gmail.com>
2026-02-01 15:12:14 +0700

    mot nguoi di tiep

--- asset/js/js.js
+++ asset/js/js.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
   $(".toggle-block").click(function () {
     $(this).toggleClass("active");
   });
+  $(".mobileMenu").click(function () {
+    $(this).toggleClass("active");
+  });
 });
